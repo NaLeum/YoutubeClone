387c61dc6d633de837ba1a25557c4e221a8d9fec
NaLeum <wognsdl@likelion.org>
2020-09-25 12:18:13 +0900

    database and get,post

--- middlewares.js
+++ middlewares.js
@@ -4,6 +4,10 @@ import routes from "./routes";
 export const localsMiddleware = (req,res,next) => {
     res.locals.siteName = 'NaleumTube';
     res.locals.routes = routes;
+    res.locals.user = {
+        isAuthenticated: true,
+        id:1
+    }
     next();
 } // locals는 로컬변수응답을 포함하는 객체입니다. 
 // locals에 있는것은 템플릿에 변수명처럼 존재한다.
