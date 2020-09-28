95e4ffa298f44eebd36ce9c4127cfa6c5a7f7c18
NaLeum <wognsdl@likelion.org>
2020-09-28 18:58:41 +0900

    MongoDB , Model

--- db.js
+++ db.js
@@ -1,50 +1,74 @@
-export const videos = [
+// export const videos = [
+//     {
+//       id: 324393,
+//       title: "Video awesome",
+//       description: "This is something I love",
+//       views: 24,
+//       videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+//       creator: {
+//         id: 121212,
+//         name: "Nicolas",
+//         email: "nico@las.com"
+//       }
+//     },
+//     {
+//       id: 1212121,
+//       title: "Video super",
+//       description: "This is something I love",
+//       views: 24,
+//       videoFile:  "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+//       creator: {
+//         id: 121212,
+//         name: "Nicolas",
+//         email: "nico@las.com"
+//       }
+//     },
+//     {
+//       id: 55555,
+//       title: "Video nice",
+//       description: "This is something I love",
+//       views: 24,
+//       videoFile:  "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+//       creator: {
+//         id: 121212,
+//         name: "Nicolas",
+//         email: "nico@las.com"
+//       }
+//     },
+//     {
+//       id: 11111,
+//       title: "Video perfect",
+//       description: "This is something I love",
+//       views: 24,
+//       videoFile:  "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+//       creator: {
+//         id: 121212,
+//         name: "Nicolas",
+//         email: "nico@las.com"
+//       }
+//     }
+//   ];
+
+// mongodb 는 데이터베이스 mogoose는 어뎁터
+// nosql database이고 규칙이 적고 엄청 유연
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL,
     {
-      id: 324393,
-      title: "Video awesome",
-      description: "This is something I love",
-      views: 24,
-      videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-      creator: {
-        id: 121212,
-        name: "Nicolas",
-        email: "nico@las.com"
-      }
-    },
-    {
-      id: 1212121,
-      title: "Video super",
-      description: "This is something I love",
-      views: 24,
-      videoFile:  "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-      creator: {
-        id: 121212,
-        name: "Nicolas",
-        email: "nico@las.com"
-      }
-    },
-    {
-      id: 55555,
-      title: "Video nice",
-      description: "This is something I love",
-      views: 24,
-      videoFile:  "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-      creator: {
-        id: 121212,
-        name: "Nicolas",
-        email: "nico@las.com"
-      }
-    },
-    {
-      id: 11111,
-      title: "Video perfect",
-      description: "This is something I love",
-      views: 24,
-      videoFile:  "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-      creator: {
-        id: 121212,
-        name: "Nicolas",
-        email: "nico@las.com"
-      }
+        useNewUrlParser: true,
+        useFindAndModify: false
     }
-  ];
+);
+
+const db = mongoose.connection;
+
+const handleOpen = () => {
+    console.log("Connected to DB")
+};
+
+const handleError = error => console.log(`Error on DB Connection:${error}`)
+
+db.once('open',handleOpen);
+db.on('error',handleError);
--- init.js
+++ init.js
@@ -1,7 +1,12 @@
+import "./db";
 import app from './app';
+import dotenv from 'dotenv';
+dotenv.config();
+import "./models/Video";
+import "./models/Comment";
 
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 // 원래는 바벨을 통해 arrow function을 사용했지만, nodejs의 버전업으로 그냥도 읽힌다.
 const handleListening = () => {
